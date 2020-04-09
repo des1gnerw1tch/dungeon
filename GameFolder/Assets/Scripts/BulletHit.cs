75f17ac851a9b17792087be32237b0b6dcc46906
ragnvall <robertagnvall@gmail.com>
2020-04-09 11:14:02 -0600

    New Player and i added aliitle bit of animation

---
+++ GameFolder/Assets/Scripts/BulletHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHit : MonoBehaviour
+{
+    public GameObject hitEffect;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, .2f);
+        Destroy(gameObject);
+    }
+}
