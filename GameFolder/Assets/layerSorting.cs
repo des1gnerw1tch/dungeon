9fb4a0b6a254d743c443a40a745c204dddce1730
Zachary Walker-Liang <zachcanon@gmail.com>
2020-04-13 18:10:29 -0400

    Trees

---
+++ GameFolder/Assets/layerSorting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class layerSorting : MonoBehaviour
+{
+  public Transform player;
+  public SpriteRenderer tree;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (player.position.y > transform.position.y - .8) {
+
+        tree.sortingOrder = 3;
+      } else  {
+
+        tree.sortingOrder = 1;
+      }
+
+    }
+}
