965fd8eed4f75d450255fe876d607df62b2c79b0
ragnvall <robertagnvall@gmail.com>
2020-04-14 09:33:40 -0600

    GunCanNow be Equipped but when droped it needs to be destroyed

---
+++ GameFolder/Assets/AttachGunToPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachGunToPlayer : MonoBehaviour
+{
+    private Transform firepointPos;
+    private EquippedGun GunScript;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        firepointPos = GameObject.FindGameObjectWithTag("FirePoint").transform;
+        GunScript = GameObject.FindGameObjectWithTag("AR").GetComponent<EquippedGun>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(GunScript.isEquipped == true){
+            firepointPos = GameObject.FindGameObjectWithTag("FirePoint").transform;
+            GunScript.Gun.transform.position = firepointPos.position;
+            GunScript.Gun.transform.rotation = firepointPos.rotation;
+		//}
+        
+    }
+}
--- GameFolder/Assets/Scripts/EquippedGun.cs
+++ GameFolder/Assets/Scripts/EquippedGun.cs
@@ -6,19 +6,19 @@ public class EquippedGun : MonoBehaviour
 {
     private Transform firepointPos;
     public GameObject GunPrefab;
-    GameObject Gun;
+    public GameObject Gun;
+    public bool isEquipped;
     public void Start()
     {
         firepointPos = GameObject.FindGameObjectWithTag("FirePoint").transform;
         Vector2 Pos = new Vector2(firepointPos.position.x, firepointPos.position.y);
         Gun = Instantiate(GunPrefab, Pos,firepointPos.rotation);
+        isEquipped = true;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        firepointPos = GameObject.FindGameObjectWithTag("FirePoint").transform;
-        Gun.transform.position = firepointPos.position;
-        Gun.transform.rotation = firepointPos.rotation;
+        
     }
 }
