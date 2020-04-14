e97dcc7e66bb8c2f233c4eb26e963fa8171b22e5
ragnvall <robertagnvall@gmail.com>
2020-04-13 23:39:09 -0600

    3

---
+++ GameFolder/Assets/EquippedGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedGun : MonoBehaviour
+{
+    private Transform firepointPos;
+    public GameObject GunPrefab;
+    GameObject Gun;
+    public void Start()
+    {
+        firepointPos = GameObject.FindGameObjectWithTag("FirePoint").transform;
+        Vector2 Pos = new Vector2(firepointPos.position.x, firepointPos.position.y);
+        Gun = Instantiate(GunPrefab, Pos,firepointPos.rotation);
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        firepointPos = GameObject.FindGameObjectWithTag("FirePoint").transform;
+        Gun.transform.position = firepointPos.position;
+        Gun.transform.rotation = firepointPos.rotation;
+    }
+}
