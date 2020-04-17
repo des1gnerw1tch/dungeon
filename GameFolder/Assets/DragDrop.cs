fd96fa3d44f9032f2a6b1785965a2a81a62e8784
ragnvall <robertagnvall@gmail.com>
2020-04-17 15:30:54 -0600

    das

---
+++ GameFolder/Assets/DragDrop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DragDrop : MonoBehaviour,IPointerDownHandler,IBeginDragHandler, IEndDragHandler{
+
+	 public void OnBeginDrag(PointerEventData eventData){
+		Debug.Log("1");
+	 }
+	 public void OnEndDrag(PointerEventData eventData){
+		Debug.Log("2");
+	 }
+	 public void OnPointerDown(PointerEventData eventData){
+		Debug.Log("3");
+	 }
+}
