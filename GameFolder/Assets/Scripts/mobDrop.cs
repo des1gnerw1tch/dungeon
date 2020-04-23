2f77e94bdd83dd044789e6730681655d95e8d3ed
ragnvall <robertagnvall@gmail.com>
2020-04-23 14:36:27 -0600

    Chests

--- GameFolder/Assets/Scripts/mobDrop.cs
+++ GameFolder/Assets/Scripts/mobDrop.cs
@@ -9,6 +9,7 @@ public class mobDrop : MonoBehaviour
 	public GameObject SnipeDropPrefab;
 	public GameObject HealthPrefab;
 	public GameObject CoinPrefab;
+	Vector3 offset;
 	public void SpiderDrop(Vector3 pos){
 		float num = Random.Range(0,100);
         if(num <= 10){
@@ -19,5 +20,23 @@ public class mobDrop : MonoBehaviour
 	}
 	public void GoblinDrop(Vector3 pos){
 		Instantiate(CoinPrefab, pos, Quaternion.identity);
+	}
+
+	public void ChestDrop(Vector3 pos){
+		
+		offset.Set(pos.x + Random.Range(-3,3), pos.y + 2f, 0f);
+		Instantiate(CoinPrefab, offset, Quaternion.identity);
+		float num = Random.Range(0,100);
+        if(num <= 10){
+            Instantiate(ARdropPrefab, offset, Quaternion.identity);
+		}else if(num <= 20 && num > 10){
+            Instantiate(HealthPrefab, offset, Quaternion.identity);
+		}else if(num <= 40 && num > 20){
+			Instantiate(CoinPrefab, offset, Quaternion.identity);
+		}else if(num <= 50 && num > 40){
+			Instantiate(SnipeDropPrefab, offset, Quaternion.identity);
+		}else if(num <= 55 && num > 50){
+			Instantiate(RPGdropPrefab, offset, Quaternion.identity);
+		}
 	}
 }
