83e3a5603d10424f1107e965898211cc898a9e59
Zachary Walker-Liang <zachcanon@gmail.com>
2020-04-27 23:35:55 -0400

    shop and save fix ?

--- GameFolder/Assets/Scripts/DontDestroy.cs
+++ GameFolder/Assets/Scripts/DontDestroy.cs
@@ -7,12 +7,12 @@ public class DontDestroy : MonoBehaviour
 
     void Awake()
     {
-      /*GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
+      GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
 
         if (objs.Length > 1)
         {
             Destroy(this.gameObject);
-        }*/
+        }
 
         DontDestroyOnLoad(this.gameObject);
     }
