2ea22473e46ff9471883fc5967f56fdbbb9c7a9c
Zachary Walker-Liang <zachcanon@gmail.com>
2020-04-29 22:39:47 -0400

    more colliders

--- GameFolder/Assets/Scripts/CaveToMain.cs
+++ GameFolder/Assets/Scripts/CaveToMain.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class CaveToMain : MonoBehaviour
 {
     public string sceneToLoad;
+    private Transform player;
+    public bool teleportToSetPosition = false;
+    public float posX;
+    public float posY;
     // Start is called before the first frame update
+
+    void Start()  {
+      player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
     void OnTriggerEnter2D(Collider2D hitInfo) {
 
     if(hitInfo.gameObject.tag == "Player"){
           SceneManager.LoadScene(sceneToLoad);
+          if (teleportToSetPosition)  {
+            Vector2 newPosition = new Vector2(posX, posY);
+            player.position = newPosition;
+          }
       }
 
     }
