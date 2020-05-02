379c2bffe35ed418acb89959eee8ceda48a667d9
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-01 22:08:42 -0400

    Random Chest Spawns

---
+++ GameFolder/Assets/Scripts/chestSpawn.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class chestSpawn : MonoBehaviour
+{
+    public GameObject chestPrefab;
+    [Range(0f, 1f)]
+    public float chanceOfSpawn;
+    void Start()
+    {
+      float num = Random.Range(0f, 1f);
+      if (num < chanceOfSpawn)  {
+        Instantiate(chestPrefab, transform.position, Quaternion.identity);
+      }
+    }
+
+
+
+}
