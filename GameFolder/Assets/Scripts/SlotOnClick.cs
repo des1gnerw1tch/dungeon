53cba21bf05227752938117a7482a5c790e1c336
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-02 15:38:04 -0400

    bug fix

--- GameFolder/Assets/Scripts/SlotOnClick.cs
+++ GameFolder/Assets/Scripts/SlotOnClick.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class SlotOnClick : MonoBehaviour
 {
-    
+
     public int slotNumber;
     private scroll scrollScript;
     void Start(){
         scrollScript = GameObject.FindGameObjectWithTag("Player").GetComponent<scroll>();
 	}
     public void ClickToEquip(){
+
         scrollScript.activeSlot = slotNumber;
         Debug.Log("clicked");
 	}
+  
 }
