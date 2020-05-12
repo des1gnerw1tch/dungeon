a3c0bc03b9e9d743b69e09c401a1c03021b47667
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-11 23:40:42 -0400

    Boss Spawns minions

---
+++ GameFolder/Assets/Scripts/MinionSpawn.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class MinionSpawn : MonoBehaviour
+{
+    public GameObject minion;
+    public int minsPerSpawn;
+    public float spawnInterval;
+    private float counter;
+
+    void Start()
+    {
+      counter = spawnInterval;
+    }
+
+    void Update()
+    {
+
+      if (counter > 0)  {
+        counter -= Time.deltaTime;
+      } else {
+        Spawn();
+        counter = spawnInterval;
+      }
+
+    }
+
+    void Spawn()  {
+      //spawns minions
+      Vector2 pos = transform.position;
+      for (int i = minsPerSpawn; i > 0 ; i--) {
+        GameObject instance = Instantiate(minion, pos, Quaternion.identity);
+        Animator animator = instance.GetComponent<Animator>();
+        animator.SetBool("isPatrolling", true);
+      }
+
+    }
+}
