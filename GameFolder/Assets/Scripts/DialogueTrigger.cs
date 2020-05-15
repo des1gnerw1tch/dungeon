18cec1af937c4638b21d8babec4e459a7296df2e
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-15 01:53:38 -0400

    Slime Dungeon Work

--- GameFolder/Assets/Scripts/DialogueTrigger.cs
+++ GameFolder/Assets/Scripts/DialogueTrigger.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    
+    public Animator NPC;
     public Dialogue dialogue;
 
     public void TriggerDialogue(){
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        if (NPC != null)  {
+          NPC.SetBool("Talking", true);
+        }
 	}
     public void EndTalk(){
         FindObjectOfType<DialogueManager>().EndDialogue();
+        if (NPC != null)  {
+          NPC.SetBool("Talking", false);
+        }
 	}
     void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
             TriggerDialogue();
 		}
-        
+
 	}
     void OnTriggerExit2D(Collider2D other){
         if(other.CompareTag("Player")){
