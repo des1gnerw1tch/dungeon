ff71f6b330a8d89831ec271f4b498f418cf65615
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-19 02:24:46 -0400

    Wall Detector for Mobs

---
+++ GameFolder/Assets/WallDetector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetector : MonoBehaviour
+{
+    private Animator animator;
+    void Start() {
+      animator = GetComponent<Animator>();
+    }
+    void OnCollisionStay2D(Collision2D other)  {
+      if (!(other.gameObject.CompareTag("Player"))) {
+        animator.SetBool("isPatrolling", false);
+      }
+    }
+}
