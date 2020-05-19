9a5ee3518ccb582a3f689f31c2416606f3116bdc
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-18 20:34:29 -0400

    Spawner Stuff?

---
+++ GameFolder/Assets/SpawnerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    Spawner[] spawners;
+    EnemyHealth[] enemies;
+    public int maxEnemies;
+    void Start()
+    {
+        spawners = FindObjectsOfType<Spawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      enemies = FindObjectsOfType<EnemyHealth>();
+      if (enemies.Length >= maxEnemies) {
+        foreach(Spawner s in spawners)  {
+          s.enabled = false;
+        }
+      } else {
+        foreach(Spawner s in spawners)  {
+          s.enabled = true;
+        }
+      }
+    }
+}
