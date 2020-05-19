d20fde7d8b2b253bd51f012731255f7c23f3a2ac
ragnvall <robertagnvall@gmail.com>
2020-05-19 17:40:08 -0600

    puzzle

---
+++ GameFolder/Assets/LightSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public GameObject Lights;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")){
+            if (Input.GetKey("e"))
+            {
+                Lights.SetActive(true);
+                Debug.Log("lights should turn on.");
+            }
+            Debug.Log("is working.");
+        }
+    }
+}
