a2a62f45006aafd1033e67bf9ce64599bf05cc5a
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-26 17:14:32 -0400

    eh cursor

--- GameFolder/Assets/UI/Cursor/mouseCursor.cs
+++ GameFolder/Assets/UI/Cursor/mouseCursor.cs
@@ -6,7 +6,8 @@ public class mouseCursor : MonoBehaviour
 {
 
     // Update is called once per frame
-
+    public Animator animator;
+    public Renderer sprite;
     void Start()  {
       Cursor.visible = false;
     }
@@ -14,5 +15,20 @@ public class mouseCursor : MonoBehaviour
     {
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = cursorPos;
+
+        if (Input.GetMouseButtonDown(0))  {
+           animator.SetBool("pressed", true);
+        }
+        if (Input.GetMouseButtonUp(0))  {
+           animator.SetBool("pressed", false);
+        }
+
+      /*  if (Input.GetAxisRaw("Mouse X") != 0|| Input.GetAxisRaw("Mouse Y") != 0) {
+          sprite.enabled = true;
+        } else {
+          sprite.enabled = false;
+        }*/
+
+        //Debug.Log(Input.GetAxisRaw("Mouse X"));
     }
 }
