3ab5cc7590c696b08d30b3bedbbf4bc679d8438a
Zachary Walker-Liang <zachcanon@gmail.com>
2020-05-25 23:11:07 -0400

    Cursor 1.o

--- GameFolder/Assets/DragInstance.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DragInstance : MonoBehaviour
-{
-    public GameObject itemDragging;
-    public Slot slotDragging;
-}
---
+++ GameFolder/Assets/UI/Cursor/mouseCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseCursor : MonoBehaviour
+{
+
+    // Update is called once per frame
+
+    void Start()  {
+      Cursor.visible = false;
+    }
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
