799f339bfaed1e6ac22f22694b852e4fa75e516f
taylorwkelly <taylorwkelly@users.noreply.github.com>
2020-06-02 15:04:43 -0600

    Stamina Update pt 1

---
+++ GameFolder/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBar : MonoBehaviour
+{
+    
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxStamina(int stam)
+    {
+        slider.maxValue = stam;
+        slider.value = stam;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetStamina(int stam)
+    {
+        slider.value = stam;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
