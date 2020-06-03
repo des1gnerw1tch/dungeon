51cedf763dfa02ddf8ffe3e863e0367e00e44b90
ragnvall <robertagnvall@gmail.com>
2020-06-03 12:04:18 -0600

    so I got it to Be all conected in the same canvas i just wanted to do the actual drag stuff togeather cux i lowkey dont understand it.

---
+++ GameFolder/Assets/vault.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class vault : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool vaultActivated = false;
+    public GameObject vaultCanvas;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown("e") && other.CompareTag("Player"))
+        {
+            if (vaultActivated)
+            {
+                //Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+    }
+    /*void Update()
+    {
+        if (vaultActivated && Input.GetKeyDown("e"))
+        {
+            Close();
+        }
+    }*/
+    void Open()
+    {
+        vaultCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        vaultActivated = true;
+        FindObjectOfType<ItemManager>().enabled = false;
+    }
+    public void Close()
+    {
+        vaultCanvas.SetActive(false);
+        Time.timeScale = 1;
+        vaultActivated = false;
+        FindObjectOfType<ItemManager>().enabled = true;
+    }
+}
---
+++ GameFolder/Assets/vaultCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vaultCanvas : MonoBehaviour
+{
+    public vault vaultScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w")|| Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
+        {
+            vaultScript.Close();
+        }
+    }
+}
