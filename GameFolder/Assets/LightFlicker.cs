7963e22a0bad9e10eea7f69a23f52c01f0723d0a
Zachary Walker-Liang <zachcanon@gmail.com>
2020-06-08 00:19:22 -0400

    Tent Scene

---
+++ GameFolder/Assets/LightFlicker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField] private Light2D light;
+    [SerializeField] private float lowFrequency;
+    [SerializeField] private float maxFrequency;
+    [SerializeField] private float lowFlickerTime;
+    [SerializeField] private float maxFlickerTime;
+
+    private float counter;
+    private float flickerTime;
+    private bool isFlickering = false;
+
+    void Start()  {
+        counter = Random.Range(lowFrequency, maxFrequency);
+        flickerTime = Random.Range(lowFlickerTime, maxFlickerTime);
+    }
+
+    void Update()
+    {
+
+      if (counter > flickerTime)  {
+        counter -= Time.deltaTime;
+      } else  {
+        if (!isFlickering)  {
+          Flicker();
+          isFlickering = true;
+        }
+        if (counter <= 0f) {
+          isFlickering = false;
+          light.intensity = 1.5f;
+          counter = Random.Range(lowFrequency, maxFrequency);
+          flickerTime = Random.Range(lowFlickerTime, maxFlickerTime);
+          //return;
+        } else {
+          counter -= Time.deltaTime;
+        }
+      }
+
+    }
+
+    void Flicker() {
+      light.intensity = Random.Range(.5f, 1f);
+    }
+}
