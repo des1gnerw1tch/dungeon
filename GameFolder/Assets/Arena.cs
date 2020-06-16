12f6cfac7f4ede61a62eb58a6a56245e0c4db9f1
ragnvall <robertagnvall@gmail.com>
2020-06-15 18:33:21 -0600

    Puzzle Scene Stuff

---
+++ GameFolder/Assets/Arena.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour
+{
+    public GameObject[] Spawners;
+    // Start is called before the first frame update
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Input.GetKey("e"))
+        {
+            Spawners[0].SetActive(true);
+        }
+
+    }
+}
