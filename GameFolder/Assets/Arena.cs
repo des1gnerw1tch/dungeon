6198ee3c018260220c6371c370f8f2261ddae4ab
ragnvall <robertagnvall@gmail.com>
2020-06-16 23:55:10 -0600

    Arena System On point

--- GameFolder/Assets/Arena.cs
+++ GameFolder/Assets/Arena.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class Arena : MonoBehaviour
 {
     public GameObject[] Spawners;
+    private int counter = 0;
     // Start is called before the first frame update
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player") && Input.GetKey("e"))
         {
-            Spawners[0].SetActive(true);
+            Spawners[counter].SetActive(true);
         }
 
+    }
+    void Update()
+    {
+        if(Spawners[counter].GetComponent<Spawner>().numAlive == Spawners[counter].GetComponent<Spawner>().max)
+        {
+            if(counter < Spawners.Length - 1)
+            {
+                Spawners[counter].SetActive(false);
+                counter += 1;
+                //Spawn loot at the tree
+                //Spawners[counter].SetActive(true);
+            }
+            else
+            {
+                Debug.Log("end of Arena");
+            }
+            
+        }
     }
 }
