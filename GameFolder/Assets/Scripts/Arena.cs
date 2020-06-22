9a0357e4650d7ca50a13a22d0a183bf09b5266dc
ragnvall <robertagnvall@gmail.com>
2020-06-21 22:53:16 -0600

    Arena Stuff

--- GameFolder/Assets/Scripts/Arena.cs
+++ GameFolder/Assets/Scripts/Arena.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Arena : MonoBehaviour
 {
     public GameObject[] Spawners;
     private int counter = 0;
+    public Text WaveText;
+    public GameObject WaveUI;
+    public bool lootgiven = false;
+    public bool WaveCompleted = true;
     // Start is called before the first frame update
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && Input.GetKey("e"))
+        if (other.CompareTag("Player") && Input.GetKey("e") && WaveCompleted)
         {
-            Spawners[counter].SetActive(true);
+            if(lootgiven)
+            {
+                Spawners[counter].SetActive(true);
+                lootgiven = false;
+                WaveCompleted = false;
+            }
+            else
+            {
+                FindObjectOfType<DropManager>().Drop("ArenaEnd", transform.position);
+                lootgiven = true;
+            }
+            
+            WaveUI.SetActive(true);
         }
 
     }
@@ -23,12 +39,18 @@ public class Arena : MonoBehaviour
             {
                 Spawners[counter].SetActive(false);
                 counter += 1;
+                WaveCompleted = true;
+                int num = counter + 1;
+                WaveText.text = "Wave " + num;
+                FindObjectOfType<DropManager>().Drop("ArenaEnd", transform.position);
                 //Spawn loot at the tree
                 //Spawners[counter].SetActive(true);
             }
             else
             {
                 Debug.Log("end of Arena");
+                FindObjectOfType<DropManager>().Drop("ArenaEnd", transform.position);
+                WaveUI.SetActive(false);
             }
             
         }
