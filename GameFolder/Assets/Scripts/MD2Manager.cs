2930736548624a77e148abdff72f97e116b1c45a
Zachary Walker-Liang <zachcanon@gmail.com>
2020-06-25 15:51:26 -0400

    Mossy Dungeon Cleanup

--- GameFolder/Assets/Scripts/MD2Manager.cs
+++ GameFolder/Assets/Scripts/MD2Manager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MD2Manager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private GameObject bossTeleporter;
+    [SerializeField] private GameObject completedDialogue;
     void Start()
     {
         UpdateScene();
@@ -13,6 +15,12 @@ public class MD2Manager : MonoBehaviour
     // Update is called once per frame
     void UpdateScene()
     {
-      
+      if (PlayerProgress.nurseFreed)  {
+        bossTeleporter.SetActive(false);
+        completedDialogue.SetActive(true);
+      } else {
+        bossTeleporter.SetActive(true);
+        completedDialogue.SetActive(false);
+      }
     }
 }
