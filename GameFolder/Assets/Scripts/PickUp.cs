619b858fdf5f8d03de1d40f0a33cea8a507125aa
Zachary Walker-Liang <zachcanon@gmail.com>
2020-06-27 18:31:48 -0400

    Item drop improvements

--- GameFolder/Assets/Scripts/PickUp.cs
+++ GameFolder/Assets/Scripts/PickUp.cs
@@ -7,12 +7,13 @@ public class PickUp : MonoBehaviour
     private Inventory inventory;
     public GameObject itemButton;
     public string inventoryID;
+    public bool wasDropped = false;
     void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
     void OnTriggerEnter2D(Collider2D other){
-        if  (other.CompareTag("Player")){
+        if  (other.CompareTag("Player") && !wasDropped){
             bool isDuplicate = false;
             for(int j = 0; j < inventory.slots.Length; j ++){
                 if(inventory.item[j] == inventoryID && inventory.item[j] != "HealthPotion" && inventory.item[j] != "Torch")
@@ -36,5 +37,11 @@ public class PickUp : MonoBehaviour
 
 	     }
 	}
+
+  void OnTriggerExit2D(Collider2D other)  {
+    if (other.CompareTag("Player")) {
+      wasDropped = false;
+    }
+  }
 
 }
