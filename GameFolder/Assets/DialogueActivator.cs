669874dbe72025f0ed622bd42b6fcee059937083
ragnvall <robertagnvall@gmail.com>
2020-07-07 19:03:38 -0600

    ceystal Cove

---
+++ GameFolder/Assets/DialogueActivator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueActivator : MonoBehaviour
+{
+    public GameObject[] DialogueTiggers;
+    // Start is called before the first frame update
+    public void ChangeDialogue()
+    {
+
+        DialogueTiggers[0].SetActive(false);
+        DialogueTiggers[1].SetActive(false);
+        DialogueTiggers[2].SetActive(true);
+    }
+}
