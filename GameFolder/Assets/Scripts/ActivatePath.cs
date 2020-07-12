356f2bc562bbee484dca7fa62f0bf73b4598edb5
ragnvall <robertagnvall@gmail.com>
2020-07-12 15:38:57 -0600

    CoveUpDate

---
+++ GameFolder/Assets/Scripts/ActivatePath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePath : MonoBehaviour
+{
+    public GameObject TileMapActivate;
+    public GameObject TileMapaDeactivate;
+    public static bool hasDropped = false;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            TileMapActivate.SetActive(true);
+            TileMapaDeactivate.SetActive(false);
+            if (!hasDropped)
+            {
+                FindObjectOfType<DropManager>().Drop("WhiteGauntlet", transform.position);
+                hasDropped = true;
+            }
+            
+        }
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
