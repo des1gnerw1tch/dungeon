0de547d747612c58d8e1ced0bbf91e8b5ca60249
ragnvall <robertagnvall@gmail.com>
2020-07-15 12:54:53 -0600

    Some crystal Stuff

--- GameFolder/Assets/Scripts/ActivatePath.cs
+++ GameFolder/Assets/Scripts/ActivatePath.cs
@@ -7,14 +7,18 @@ public class ActivatePath : MonoBehaviour
     public GameObject TileMapActivate;
     public GameObject TileMapaDeactivate;
     public static bool hasDropped = false;
+    public bool ShouldDrop;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
             TileMapActivate.SetActive(true);
-            TileMapaDeactivate.SetActive(false);
-            if (!hasDropped)
+            if (TileMapaDeactivate != null) { 
+                TileMapaDeactivate.SetActive(false);
+            }
+            
+            if (!hasDropped && ShouldDrop)
             {
                 FindObjectOfType<DropManager>().Drop("WhiteGauntlet", transform.position);
                 hasDropped = true;
