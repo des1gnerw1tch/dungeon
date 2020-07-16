4504bb532979f421086b1eb3f6850bba10050fd2
ragnvall <robertagnvall@gmail.com>
2020-07-15 18:33:22 -0600

    teleportGun now works idk why it didnt before but you can shoot more than 2

--- GameFolder/Assets/Teleportation.cs
+++ GameFolder/Assets/Teleportation.cs
@@ -7,10 +7,15 @@ public class Teleportation : MonoBehaviour
 
     private Transform player;
     public bool HasbeenDestroyed;
+    public static int counterID;
+    public int id;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset.Set(0, 2, 0);
+        counterID += 1;
+        id = counterID;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         
     }
@@ -18,11 +23,22 @@ public class Teleportation : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
+        
+        Debug.Log(id);
         if (other.gameObject.tag == "Player")
         {
-            player.position = FindObjectOfType<Teleport>().position1;
+            
+            if(id == 1)
+            {
+                player.position = FindObjectOfType<Teleport>().position2 - offset;
+            }
+            if(id == 2)
+            {
+                player.position = FindObjectOfType<Teleport>().position1- offset;
+            }
+            
             FindObjectOfType<Teleport>().Counter += 1;
-            Destroy(gameObject);
+            //Destroy(gameObject);
             
         }
     }
