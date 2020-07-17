564b0cfc002f2a03b6a27fcfbaa4ab37b1fcfaac
ragnvall <robertagnvall@gmail.com>
2020-07-17 11:09:26 -0600

    2nd door

---
+++ GameFolder/Assets/TargetPractace.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPractace : MonoBehaviour
+{
+    public GameObject lights;
+    public TargetPractaceManager TargetPractaceManagerScript;
+    public static int counter;
+    public static bool IsLit;
+    public int ID;
+    //public int randomNumber;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("CrystalShot") && IsLit)
+        {
+            counter++;
+            Debug.Log("counter");
+        }
+    }
+    public void LightSpawn(GameObject lightSpawn) {
+        //Instantiate(lightSpawn, transform.position, Quaternion.identity);
+        lightSpawn.SetActive(true);
+    }
+    void Update()
+    {
+        //Invoke("ChooseNumber", 2f);
+        //Debug.Log(randomNumber);
+        if(TargetPractaceManagerScript.numTurn == ID){
+            lights.SetActive(true);
+            //IsLit = true;
+        }
+        else
+        {
+            lights.SetActive(false);
+        }
+    }
+    //public void ChooseNumber(){
+     //   randomNumber = Random.Range(0, 5); 
+    //}
+}
---
+++ GameFolder/Assets/TargetPractaceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPractaceManager : MonoBehaviour
+{
+    public int numTurn;
+    public bool isWaiting;
+    void Update()
+    {
+        if (isWaiting)
+        {
+            Debug.Log(numTurn);
+        }
+        else
+        {
+            StartCoroutine(ChooseNumber());
+        }
+        
+        /*Debug.Log(randomNumber);
+        if (randomNumber == ID)
+        {
+            LightSpawn(ligthts);
+            IsLit = true;
+        }*/
+    }
+    IEnumerator ChooseNumber()
+    {
+        // suspend execution for 5 seconds
+        isWaiting = true;
+        yield return new WaitForSeconds(5);
+        isWaiting = false;
+        numTurn = Random.Range(0, 5);
+    }
+}
