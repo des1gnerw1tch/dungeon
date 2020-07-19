56bd75ca31355d881bd2caa0984d99d6c27f594f
Zachary Walker-Liang <zachcanon@gmail.com>
2020-07-19 13:57:54 -0400

    Salvage Completed (need transitions)

--- GameFolder/Assets/DeathContinue.cs
+++ GameFolder/Assets/DeathContinue.cs
@@ -16,6 +16,14 @@ public class DeathContinue : MonoBehaviour
       player.GetComponent<PlayerHealth>().currentHealth = 20;
       Vector3 reset = new Vector3(24.35f, 22.94f, 0f);
       player.transform.position = reset;
+      Inventory inventory = player.GetComponent<Inventory>();
+      for (int i = 0; i < 5; i++)  {
+        if (!deathInventory.slotSelected[i])  {
+          // destroy item
+          inventory.item[i] = null;
+          inventory.slots[i].GetComponent<Slot>().DestroyItem();
+        }
+      }
       SceneManager.LoadScene("Main");
       //player.GetComponent<PlayerHealth>().currentHealth = 20;
       //Debug.Log(player.GetComponent<PlayerHealth>().currentHealth);
