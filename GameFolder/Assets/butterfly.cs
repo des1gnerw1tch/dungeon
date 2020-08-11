fe8931fef0ea00d8c78b08aba6bc1df1ff4c08ef
Zachary Walker-Liang <zachcanon@gmail.com>
2020-08-11 00:06:12 -0400

    overworld animated tiles and butterflies

---
+++ GameFolder/Assets/butterfly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class butterfly : MonoBehaviour
+{
+    private Transform player;
+    [SerializeField]
+    private float despawnDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+      player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Vector2 butterfly = new Vector2(transform.position.x, transform.position.y);
+      Vector2 playerPos = new Vector2(player.position.x, player.position.y);
+      if (Mathf.Abs(Vector2.Distance(butterfly, playerPos)) > despawnDistance)  {
+        Destroy(this.gameObject);
+      }
+    }
+}
