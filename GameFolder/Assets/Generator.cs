e6464483e00a9ab815f4296dfd35677e8f466dd6
Zachary Walker-Liang <zachcanon@gmail.com>
2020-08-12 21:37:02 -0400

    Control room coding start

---
+++ GameFolder/Assets/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    void Start()  {
+      if (LavaDungManager.powerOn)  {
+        Destroy(this.gameObject);
+      }
+    }
+    [SerializeField]
+    private DialogueTrigger onDialogue;
+    void OnTriggerStay2D(Collider2D other)  {
+      if (other.CompareTag("Player")) {
+        if (Input.GetKeyDown("e"))  {
+          LavaDungManager.powerOn = true;
+          onDialogue.TriggerDialogue();
+          Destroy(this.gameObject);
+        }
+      }
+    }
+}
