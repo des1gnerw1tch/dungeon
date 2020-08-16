a3dfae9c19459bb2732dba97374cf66936458cf3
Zachary Walker-Liang <zachcanon@gmail.com>
2020-08-16 14:30:42 -0400

    Siren Stops when exiting dungeon

--- GameFolder/Assets/LavaCaveManager.cs
+++ GameFolder/Assets/LavaCaveManager.cs
@@ -10,5 +10,6 @@ public class LavaCaveManager : MonoBehaviour
       /*turns off the power in the lava dungeon if turned on, this is here to make sure that
       when you leave the dungeon scene, lava dung progress is reset. */
       LavaDungManager.powerOn = false;
+      FindObjectOfType<AudioManager>().Stop("siren");
     }
 }
