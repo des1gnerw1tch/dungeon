1d7b4fe7ce0f7eb6ed5face0c39e4b88e5fe52d9
ragnvall <robertagnvall@gmail.com>
2020-08-17 18:12:42 -0600

    pathfinding on the hounds.

---
+++ GameFolder/Assets/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
