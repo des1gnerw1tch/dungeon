90de341a604f2ac6f099319fcb2003b4fd82880d
Zachary Walker-Liang <zachcanon@gmail.com>
2020-08-20 00:54:01 -0400

    Friend Stays in Tent with you

--- GameFolder/Assets/UI/TentManager.cs
+++ GameFolder/Assets/UI/TentManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class TentManager : MonoBehaviour
 {
     public VaultHolder vaultHolder;
+    public GameObject friendNPC;
     void Start()
     {
       vaultHolder = FindObjectOfType<VaultHolder>();
       vaultHolder.EnableVault();
+      if (PlayerProgress.friendFreed) {
+        friendNPC.SetActive(true);
+      } else {
+        friendNPC.SetActive(false);
+      }
     }
 
 }
