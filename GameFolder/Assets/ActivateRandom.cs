9a05d268cb4034dedb48b58f02be01c3711642ef
Zachary Walker-Liang <zachcanon@gmail.com>
2020-08-24 02:14:42 -0400

    New ecco dialogue

---
+++ GameFolder/Assets/ActivateRandom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateRandom : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] objs;
+    // Start is called before the first frame update
+    void Start()
+    {
+      int num = Random.Range(0, objs.Length);
+      objs[num].SetActive(true);
+    }
+
+}
