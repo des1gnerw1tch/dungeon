0a6c0d550987cd44b93312761777950d1fa11ac5
Zachary Walker-Liang <zachcanon@gmail.com>
2020-08-25 00:17:36 -0400

    Fixed dragging glitch
    Fixed dragging glitch where you could consume items while reorganizing inventory

---
+++ GameFolder/Assets/SlotCover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class SlotCover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    /* this slot cover script will make it so that you cannot shoot or use items when hovering over it.
+    this allows you to drag and drop items without shooting in between*/
+
+    [SerializeField]
+    private ItemManager canShoot;
+    
+    public void OnPointerEnter(PointerEventData eventData)  {
+      canShoot.enabled = false;
+    }
+    public void OnPointerExit(PointerEventData eventData)  {
+      canShoot.enabled = true;
+    }
+}
