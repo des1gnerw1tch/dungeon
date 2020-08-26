82355279d08feeb186c3a9471d3609b3efc53a1f
ragnvall <robertagnvall@gmail.com>
2020-08-26 10:51:56 -0600

    fixed up the lava gun try it out its pretty cool

---
+++ GameFolder/Assets/LavaConstDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaConstDamage : MonoBehaviour
+{
+    private bool ShouldCauseDamage = true;
+    [SerializeField] private int damage;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<EnemyHealth>() != null && ShouldCauseDamage) {
+            StartCoroutine(CauseDamage(collision));
+        }
+    }
+    private IEnumerator CauseDamage(Collider2D other)
+    {
+        ShouldCauseDamage = false;
+        other.GetComponent<EnemyHealth>().TakeDamage(damage, false);
+        yield return new WaitForSeconds(.3f);
+        ShouldCauseDamage = true;
+    }
+}
