cc3e1bc225905797370d1e06ee5a885e5c10cbb9
Zachary Walker-Liang <zachcanon@gmail.com>
2020-08-27 00:33:38 -0400

    Final music for crystal 3rd door

---
+++ GameFolder/Assets/StartMusicOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMusicOnTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject musicPlayer;
+
+    void OnTriggerEnter2D(Collider2D other) {
+      if (other.CompareTag("Player")) {
+        musicPlayer.SetActive(true);
+      }
+    }
+}
