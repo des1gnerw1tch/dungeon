da5640b5c2c088dc54dae1cec9460b2e4355e2d1
Zachary Walker-Liang <zachcanon@gmail.com>
2020-09-19 02:18:15 -0400

    Heart Float

--- GameFolder/Assets/Scripts/Heart.cs
+++ GameFolder/Assets/Scripts/Heart.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Heart : MonoBehaviour
 {
+    private Transform player;
+    [SerializeField]
+    private float magnetSpeed;
+    [SerializeField]
+    private float magnetDistance;
+    void Start()  {
+      player = FindObjectOfType<PlayerMovement>().gameObject.GetComponent<Transform>();
+    }
     void OnTriggerEnter2D(Collider2D other) {
       if(other.CompareTag("Player"))  {
         FindObjectOfType<PlayerHealth>().currentHealth += 20;
         FindObjectOfType<AudioManager>().Play("Heart");
         Destroy(gameObject);
+      }
+    }
+
+    void FixedUpdate()  {
+      if (Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(player.position.x, player.position.y)) < magnetDistance) {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, magnetSpeed* Time.fixedDeltaTime) ;
       }
     }
 }
