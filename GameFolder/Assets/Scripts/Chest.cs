d9924c33f54f814c7b09248209685262514bcb55
Zachary Walker-Liang <zachcanon@gmail.com>
2020-09-24 01:06:26 -0400

    Chest open pop up

--- GameFolder/Assets/Scripts/Chest.cs
+++ GameFolder/Assets/Scripts/Chest.cs
@@ -10,12 +10,14 @@ public class Chest : MonoBehaviour
     public string chestID;
     private Animator animator;
     public bool isActive = true;
+    private DialogueTrigger dialogue;
     // Start is called before the first frame update!!!!
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         IsNeerInteractableScript = GameObject.FindGameObjectWithTag("Player").GetComponent<IsNeerInteractable>();
         animator = GetComponent<Animator>();
+        dialogue = GetComponent<DialogueTrigger>();
     }
 
     // Update is called once per frame
@@ -39,6 +41,7 @@ public class Chest : MonoBehaviour
     void dropLoot() {
       FindObjectOfType<DropManager>().Drop(chestID, transform.position);
       FindObjectOfType<AudioManager>().Play("chest");
+      Destroy(dialogue);
     }
 
 }
