e935139ca25ceff9bd19bd67dfb5d3a8a2ab5edc
ragnvall <robertagnvall@gmail.com>
2020-09-25 12:17:36 -0600

    markers for mossy and started on slime

---
+++ GameFolder/Assets/MossySceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossySceneManager : MonoBehaviour
+{
+
+    public GameObject[] Markers;
+    public bool ShouldActivate = false;
+    private bool hasDone = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerProgress.nurseFreed && !PlayerProgress.wizardFreed)
+        {
+            Markers[0].SetActive(false);
+            hasDone = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerProgress.nurseFreed && !PlayerProgress.wizardFreed && !hasDone)
+        {
+            Markers[0].SetActive(false);
+            if(ShouldActivate)
+            {
+                Markers[1].SetActive(true);
+            }
+        }
+    }
+}
--- GameFolder/Assets/Scripts/Rotation.cs
+++ GameFolder/Assets/Scripts/Rotation.cs
@@ -23,14 +23,11 @@ public class Rotation : MonoBehaviour
     {
        
 
-        if(SceneManager.GetActiveScene().name == "Cave")
+        if(GameObject.FindGameObjectWithTag("ObjMarker") != null)
         {
-            if (!hasLooked)
-            {
-                Obj = GameObject.FindGameObjectWithTag("ObjMarker").transform;
-                hasLooked = true;
-            }
             
+            Obj = GameObject.FindGameObjectWithTag("ObjMarker").transform;
+
             Vector2 lookDir = player.position - Obj.position;
             float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90;
             transform.eulerAngles = new Vector3(0, 0, angle);
@@ -47,7 +44,7 @@ public class Rotation : MonoBehaviour
         else
         {
             miniMapArrow.SetActive(false);
-            hasLooked = false;
+            
         }
     }
 }
