db9d15502491a53f54463053113f6dd4567b28f9
Zachary Walker-Liang <zachcanon@gmail.com>
2020-10-24 20:28:25 -0400

    Goblin Boss Range v3

---
+++ GameFolder/Assets/goblinKing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goblinKing : MonoBehaviour
+{
+    [SerializeField] private EnemyHealth health;
+    [SerializeField] private RuntimeAnimatorController phase2Animator;
+    private bool phase2Triggered = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (health.GetHealth() <= health.maxHealth/2 && !phase2Triggered) {
+        GetComponent<Animator>().runtimeAnimatorController = phase2Animator;
+      }
+    }
+}
