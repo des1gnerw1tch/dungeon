f7bc29b80c91daaaa1ec301d58c1fa961cf1220f
Zachary Walker-Liang <zachcanon@gmail.com>
2020-10-25 21:06:10 -0400

    Goblin boss transition phase dialogue

--- GameFolder/Assets/goblinKing.cs
+++ GameFolder/Assets/goblinKing.cs
@@ -8,6 +8,7 @@ public class goblinKing : MonoBehaviour
     [SerializeField] private RuntimeAnimatorController phase2Animator;
     [SerializeField] private Animator animator;
     [SerializeField] private float timeCrying;
+    [SerializeField] private DialogueTrigger cryingDialogue;
 
     private bool phase2Triggered = false;
 
@@ -15,11 +16,13 @@ public class goblinKing : MonoBehaviour
     void Update()
     { // when goblin health is at half, activate second stage
       if (health.GetHealth() <= health.maxHealth/2 && !phase2Triggered) {
+        cryingDialogue.TriggerDialogue();
         GetComponent<Animator>().runtimeAnimatorController = phase2Animator;
         StartCoroutine(StopCrying());
+        phase2Triggered = true;
       }
     }
-    
+
     //this is how long he will be invincible for, and also crying
     IEnumerator StopCrying() {
       yield return new WaitForSeconds(timeCrying);
