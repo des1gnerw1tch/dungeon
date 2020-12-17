6defbe874d8333a15d78dc4372b476373a2dbf5b
Zachary Walker-Liang <zachcanon@gmail.com>
2020-12-16 20:34:58 -0500

    Fixed ruby gauntlet exploits
    Main menu or quit game would allow player to cheese past last level.

--- GameFolder/Assets/Scripts/Inventory.cs
+++ GameFolder/Assets/Scripts/Inventory.cs
@@ -13,6 +13,7 @@ public class Inventory : MonoBehaviour
     [SerializeField] private GameObject pistol;
 
 
+
     void Start() {
       item = new string[26];
       //if it is a new game, do not load
@@ -26,6 +27,19 @@ public class Inventory : MonoBehaviour
         beginningDialogue.TriggerDialogue();
         Instantiate(pistol, new Vector2(-7.33f, 26.38f), Quaternion.identity);
       }
+
+      /*this checks for a red gauntlet, and only lets the player keep it if
+      they has destroyed the red crystal. this makes sure that people don't quit
+      the game and restart to keep the red gauntlet and cheese the last level.*/
+      for (int i = 0; i < item.Length; i++) {
+          if (item[i] == "Ruby Gauntlet" && !PlayerProgress.redCrystalDestroyed)
+          {
+              slots[i].GetComponent<Slot>().DestroyItem();
+              item[i] = null;
+              Debug.Log("Destroyed illegal ruby gauntlet");
+          }
+      }
+
 
     }
 
